43107f51ba9a6f2462e1d8000eafeb18c5c18866
OmriHarary <omriharary@hotmail.com>
2023-01-27 13:50:45 -0500

    Set up as Spring Boot app
    - Generated with Spring Initializer
    - Included Kafka dependencies

---
+++ src/main/java/io/github/aaejo/institutionfinder/InstitutionFinderApplication.java
@@ -0,0 +1,13 @@
+package io.github.aaejo.institutionfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstitutionFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstitutionFinderApplication.class, args);
+	}
+
+}
