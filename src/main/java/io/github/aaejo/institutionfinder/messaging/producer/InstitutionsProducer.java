2a18d8ebceb72a6d337daf0b8b18a2c4a7c7b988
OmriHarary <omriharary@hotmail.com>
2023-02-09 12:20:08 -0500

    Kafka framework implementation
    - Introduced Kafka producer component using common record types for serialization
    - Added basic topic configuration, can be further configured if needed
    - Dummy message sending implementation until institution code is done

---
+++ src/main/java/io/github/aaejo/institutionfinder/messaging/producer/InstitutionsProducer.java
@@ -0,0 +1,35 @@
+package io.github.aaejo.institutionfinder.messaging.producer;
+
+import java.util.concurrent.CompletableFuture;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Component;
+
+import io.github.aaejo.messaging.records.Institution;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public class InstitutionsProducer {
+
+    private static final String TOPIC = "institutions";
+
+    private final KafkaTemplate<String, Institution> template;
+
+    public InstitutionsProducer(KafkaTemplate<String, Institution> template) {
+        this.template = template;
+    }
+
+    public void send(final Institution institution) {
+        CompletableFuture<SendResult<String, Institution>> sendResultFuture = this.template.send(TOPIC, institution);
+        sendResultFuture.whenComplete((result, ex) -> {
+            if (ex == null) {
+                log.debug("Sent: " + institution.toString());
+            }
+            else {
+                log.error("Failed to send: " + institution.toString(), ex);
+            }
+        });
+    }
+}
