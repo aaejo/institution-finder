1893746c1c784a90e6cceb58c519af737b0a60e6
OmriHarary <omriharary@hotmail.com>
2023-02-21 21:28:31 -0500

    First test cases for USAInstitutionFinder
    - Using static HTML files
    - Most of key success cases

--- src/main/java/io/github/aaejo/institutionfinder/finder/USAInstitutionFinder.java
+++ src/main/java/io/github/aaejo/institutionfinder/finder/USAInstitutionFinder.java
@@ -18,11 +18,11 @@ public class USAInstitutionFinder implements InstitutionFinder {
     private final InstitutionsProducer institutionsProducer;
     private final Connection registryConnection;
 
-    // public static final String[] STATES = { "AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "DC", "FL", "GA", "HI", "ID",
-    //         "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ",
-    //         "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV",
-    //         "WI", "WY", "AS", "FM", "GU", "MH", "MP", "PW", "PR", "VI" };
-    public static final String[] STATES = { "AL", "NY", "AS" };
+    public static final String[] STATES = { "AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "DC", "FL", "GA", "HI", "ID",
+            "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ",
+            "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV",
+            "WI", "WY", "AS", "FM", "GU", "MH", "MP", "PW", "PR", "VI" };
+
     public static final String PROGRAMS = "38.0104+" +  // Applied and Professional Ethics
                                           "38.0103+" +  // Ethics
                                           "38.0102+" +  // Logic
@@ -39,65 +39,69 @@ public class USAInstitutionFinder implements InstitutionFinder {
         log.info("Producing institutions for {} US states and/or territories", STATES.length);
 
         for (String state : STATES) {
-            log.info("Producing for state = {}", state);
-            int pageNum = 1;
-            boolean hasNextPage = false;
-
-            do {
-                Document page;
-                try {
-                    page = registryConnection
-                            .newRequest()
-                            .data("p", PROGRAMS)
-                            .data("s", state)
-                            .data("pg", Integer.toString(pageNum))
-                            .get();
-                } catch (IOException e) {
-                    log.error("Failed to connect to College Navigator with state = {}", state, e);
-                    continue;
-                }
-
-                Element resultsTableBody = page
-                        .getElementById("ctl00_cphCollegeNavBody_ucResultsMain_tblResults").firstElementChild();
-                Element pagingControls = page.getElementById("ctl00_cphCollegeNavBody_ucResultsMain_divPagingControls");
-
-                if (resultsTableBody == null) {
-                    log.info("No results on page");
-                    hasNextPage = false;
-                    continue;
-                } else if (pagingControls.selectFirst(":containsOwn(Next Page »)") != null) {
-                    log.info("Another page of results exists");
-                    hasNextPage = true;
-                    pageNum++;
-                } else {
-                    log.info("Final page of results reached");
-                    hasNextPage = false;
-                }
-
-                Elements results = resultsTableBody.select(".resultsW, .resultsY");
-                log.info("{} results on page", results.size());
-
-                for (Element result : results) {
-                    Element schoolInfoLink = result
-                                            .child(1) // 0 = info button, 1 = school page link, 2 = add button
-                                            .getElementsByAttribute("href")
-                                            .first();
-                    String schoolName = schoolInfoLink.text();
-                    String schoolId = Arrays.stream(schoolInfoLink.attr("href").split("&"))
-                            .filter(p -> p.startsWith("id") || p.startsWith("?id"))
-                            .findFirst().get().split("=")[1];
-
-                    log.info("{} id = {}", schoolName, schoolId);
-
-                    Institution institution = getInstitutionDetails(schoolName, schoolId);
-                    if (institution != null) {
-                        institutionsProducer.send(institution);
-                    }
-                }
-            } while (hasNextPage);
+            produceStateInstitutions(state);
         }
 
         log.info("Done");
+    }
+
+    public void produceStateInstitutions(String state) {
+        log.info("Producing for state = {}", state);
+        int pageNum = 1;
+        boolean hasNextPage = false;
+
+        do {
+            Document page;
+            try {
+                page = registryConnection
+                        .newRequest()
+                        .data("p", PROGRAMS)
+                        .data("s", state)
+                        .data("pg", Integer.toString(pageNum))
+                        .get();
+            } catch (IOException e) {
+                log.error("Failed to connect to College Navigator with state = {}", state, e);
+                continue;
+            }
+
+            Element resultsTableBody = page
+                    .getElementById("ctl00_cphCollegeNavBody_ucResultsMain_tblResults").firstElementChild();
+            Element pagingControls = page.getElementById("ctl00_cphCollegeNavBody_ucResultsMain_divPagingControls");
+
+            if (resultsTableBody == null) {
+                log.info("No results on page");
+                hasNextPage = false;
+                continue;
+            } else if (pagingControls.selectFirst(":containsOwn(Next Page »)") != null) {
+                log.info("Another page of results exists");
+                hasNextPage = true;
+                pageNum++;
+            } else {
+                log.info("Final page of results reached");
+                hasNextPage = false;
+            }
+
+            Elements results = resultsTableBody.select(".resultsW, .resultsY");
+            log.info("{} results on page", results.size());
+
+            for (Element result : results) {
+                Element schoolInfoLink = result
+                                        .child(1) // 0 = info button, 1 = school page link, 2 = add button
+                                        .getElementsByAttribute("href")
+                                        .first();
+                String schoolName = schoolInfoLink.text();
+                String schoolId = Arrays.stream(schoolInfoLink.attr("href").split("&"))
+                        .filter(p -> p.startsWith("id") || p.startsWith("?id"))
+                        .findFirst().get().split("=")[1];
+
+                log.info("{} id = {}", schoolName, schoolId);
+
+                Institution institution = getInstitutionDetails(schoolName, schoolId);
+                if (institution != null) {
+                    institutionsProducer.send(institution);
+                }
+            }
+        } while (hasNextPage);
     }
 
     public Institution getInstitutionDetails(String schoolName, String schoolId) {
---
+++ src/test/java/io/github/aaejo/institutionfinder/unit/finder/USAInstitutionFinderTests.java
@@ -0,0 +1,144 @@
+package io.github.aaejo.institutionfinder.unit.finder;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.junit.jupiter.api.Test;
+
+import io.github.aaejo.institutionfinder.finder.USAInstitutionFinder;
+import io.github.aaejo.institutionfinder.messaging.producer.InstitutionsProducer;
+import io.github.aaejo.messaging.records.Institution;
+
+public class USAInstitutionFinderTests {
+
+    private final InstitutionsProducer institutionsProducer = mock(InstitutionsProducer.class);
+    private final Connection connection = mock(Connection.class, RETURNS_DEEP_STUBS);
+
+    private final USAInstitutionFinder usaFinder = new USAInstitutionFinder(institutionsProducer, connection);
+
+    @Test
+    void getInstitutionDetails_success_correctDetailsReturned() throws IOException {
+        setupSchoolInfoMock();
+        String schoolName = "University of California-Berkeley";
+        String schoolId = "110635";
+        Institution expected = new Institution(
+            "University of California-Berkeley",
+            "USA",
+            "200 California Hall, Berkeley, California 94720",
+            "www.berkeley.edu/");
+
+        Institution actual = usaFinder.getInstitutionDetails(schoolName, schoolId);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void produceStateInstitutions_noResults_sucessfulProcessing() throws IOException {
+        setupSchoolInfoMock();
+        setupASMock();
+
+        usaFinder.produceStateInstitutions("AS");
+
+        verify(institutionsProducer, never()).send(any(Institution.class));
+    }
+
+    @Test
+    void produceStateInstitutions_singlePageResults_sucessfulProcessing() throws IOException {
+        setupSchoolInfoMock();
+        setupALMock();
+
+        usaFinder.produceStateInstitutions("AL");
+
+        verify(institutionsProducer, times(9)).send(any(Institution.class));
+    }
+
+    @Test
+    void produceStateInstitutions_multiPageResults_sucessfulProcessing() throws IOException {
+        setupSchoolInfoMock();
+        setupNYMock();
+
+        usaFinder.produceStateInstitutions("NY");
+
+        verify(institutionsProducer, times(77)).send(any(Institution.class));
+    }
+
+    /**
+     * Setup mock response for querying school info. Using UC Berkeley in all cases.
+     */
+    private void setupSchoolInfoMock() throws IOException {
+        when(connection
+                .newRequest()
+                .data(eq("id"), anyString())
+                .get())
+            .thenReturn(Jsoup.parse(
+                    new File("src/test/resources/collegenavigator/110635.html"),
+                    "UTF-8",
+                    "https://nces.ed.gov/collegenavigator/"));
+    }
+
+    /**
+     * Setup mock to throw IOException when querying school info.
+     */
+    private void setupSchoolInfoExceptionMock() throws IOException {
+        when(connection
+                .newRequest()
+                .data(eq("id"), anyString())
+                .get())
+            .thenThrow(new IOException());
+    }
+
+    /**
+     * Setup mock response for querying AS institutions.
+     */
+    private void setupASMock() throws IOException {
+        when(connection
+                .newRequest()
+                .data("p", USAInstitutionFinder.PROGRAMS)
+                .data("s", "AS")
+                .data("pg", "1")
+                .get())
+            .thenReturn(Jsoup.parse(
+                    new File("src/test/resources/collegenavigator/AS.html"),
+                    "UTF-8",
+                    "https://nces.ed.gov/collegenavigator/"));
+    }
+
+    /**
+     * Setup mock response for querying AL institutions.
+     */
+    private void setupALMock() throws IOException {
+        when(connection
+                .newRequest()
+                .data("p", USAInstitutionFinder.PROGRAMS)
+                .data("s", "AL")
+                .data("pg", "1")
+                .get())
+            .thenReturn(Jsoup.parse(
+                    new File("src/test/resources/collegenavigator/AL.html"),
+                    "UTF-8",
+                    "https://nces.ed.gov/collegenavigator/"));
+    }
+
+    /**
+     * Setup mock response for querying NY institutions. Multiple pages of results
+     */
+    private void setupNYMock() throws IOException {
+        for (int i = 1; i <= 6; i++) {
+            when(connection
+                    .newRequest()
+                    .data("p", USAInstitutionFinder.PROGRAMS)
+                    .data("s", "NY")
+                    .data("pg", Integer.toString(i))
+                    .get())
+                .thenReturn(Jsoup.parse(
+                        new File("src/test/resources/collegenavigator/NY" + i + ".html"),
+                        "UTF-8",
+                        "https://nces.ed.gov/collegenavigator/"));
+        }
+    }
+}
