7674e0748a4b4e26de15ac5cb0373aa1b6a03d4e
OmriHarary <omriharary@hotmail.com>
2023-02-21 20:32:23 -0500

    JsonInstitutionFinder test cases

--- src/test/java/io/github/aaejo/institutionfinder/unit/finder/JsonInstitutionFinderTests.java
+++ src/test/java/io/github/aaejo/institutionfinder/unit/finder/JsonInstitutionFinderTests.java
@@ -1,9 +1,11 @@
 package io.github.aaejo.institutionfinder.unit.finder;
 
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static org.mockito.Mockito.mock;
-import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.verify;
 
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 
 import org.junit.jupiter.api.Test;
@@ -12,6 +14,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.github.aaejo.institutionfinder.finder.JsonInstitutionFinder;
 import io.github.aaejo.institutionfinder.messaging.producer.InstitutionsProducer;
+import io.github.aaejo.messaging.records.Institution;
 
 public class JsonInstitutionFinderTests {
 
@@ -22,7 +25,7 @@ public class JsonInstitutionFinderTests {
             institutionsProducer, objectMapper);
 
     @Test
-    void notJsonList_illegalStateException() {
+    void produceInstitutionsJson_notJsonList_illegalStateException() {
         String json = """
                 {
                     \"name\": \"Acadia University\",
@@ -36,4 +39,71 @@ public class JsonInstitutionFinderTests {
                 .isThrownBy(() -> jsonInstitutionFinder.produceInstitutionsJson(jsonStream));
     }
 
+    @Test
+    void produceInstitutionsJson_singleItem_oneInstitutionSent() throws IOException {
+        String json = """
+                [{
+                    \"name\": \"Acadia University\",
+                    \"country\": \"Canada\",
+                    \"address\": \"15 University Ave, Wolfville, NS, B4P 2R6\",
+                    \"website\": \"https://philosophy.acadiau.ca/facstaff.html\"
+                }]""";
+        InputStream jsonStream = new ByteArrayInputStream(json.getBytes());
+        Institution expected = new Institution(
+                "Acadia University",
+                "Canada",
+                "15 University Ave, Wolfville, NS, B4P 2R6",
+                "https://philosophy.acadiau.ca/facstaff.html");
+
+        jsonInstitutionFinder.produceInstitutionsJson(jsonStream);
+
+        verify(institutionsProducer).send(expected);
+    }
+
+    @Test
+    void produceInstitutionsJson_threeItems_threeInstitutionsSent() throws IOException {
+        String json = """
+                [
+                    {
+                        \"name\": \"Acadia University\",
+                        \"country\": \"Canada\",
+                        \"address\": \"15 University Ave, Wolfville, NS, B4P 2R6\",
+                        \"website\": \"https://philosophy.acadiau.ca/facstaff.html\"
+                    },
+                    {
+                        \"name\": \"University of Ottawa\",
+                        \"country\": \"Canada\",
+                        \"address\": \"75 Laurier Ave E, Ottawa, ON, K1N 6N5\",
+                        \"website\": \"https://www.uottawa.ca/faculty-arts/philosophy\"
+                    },
+                    {
+                        \"name\": \"Brandon University\",
+                        \"country\": \"Canada\",
+                        \"address\": \"270 18th St, Brandon, MB, R7A 6A9\",
+                        \"website\": \"https://www.brandonu.ca/philosophy/faculty/\"
+                    }
+                ]""";
+        InputStream jsonStream = new ByteArrayInputStream(json.getBytes());
+        Institution expected1 = new Institution(
+                "Acadia University",
+                "Canada",
+                "15 University Ave, Wolfville, NS, B4P 2R6",
+                "https://philosophy.acadiau.ca/facstaff.html");
+        Institution expected2 = new Institution(
+                "University of Ottawa",
+                "Canada",
+                "75 Laurier Ave E, Ottawa, ON, K1N 6N5",
+                "https://www.uottawa.ca/faculty-arts/philosophy");
+        Institution expected3 = new Institution(
+                "Brandon University",
+                "Canada",
+                "270 18th St, Brandon, MB, R7A 6A9",
+                "https://www.brandonu.ca/philosophy/faculty/");
+
+        jsonInstitutionFinder.produceInstitutionsJson(jsonStream);
+
+        verify(institutionsProducer).send(expected1);
+        verify(institutionsProducer).send(expected2);
+        verify(institutionsProducer).send(expected3);
+    }
 }
