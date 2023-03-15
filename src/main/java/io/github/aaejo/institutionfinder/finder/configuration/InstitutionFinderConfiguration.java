7bd54582512ebc2289505c4e312ff8920a24cd1f
OmriHarary <omriharary@hotmail.com>
2023-03-15 14:39:35 -0400

    Import cleanup

--- src/main/java/io/github/aaejo/institutionfinder/finder/configuration/InstitutionFinderConfiguration.java
+++ src/main/java/io/github/aaejo/institutionfinder/finder/configuration/InstitutionFinderConfiguration.java
@@ -7,8 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.retry.backoff.FixedBackOffPolicy;
-import org.springframework.retry.policy.SimpleRetryPolicy;
 import org.springframework.retry.support.RetryTemplate;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
